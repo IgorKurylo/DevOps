9cd0b50fd44d42161a337676edfeed1ce81adbd7
Igork <igork@korentec.co.il>
2021-11-23 12:41:27 +0200

    remove thresold from alarm req per target, is only work when request invoke

--- terraform/modules/application/alarm.tf
+++ terraform/modules/application/alarm.tf
@@ -10,7 +10,6 @@ resource "aws_cloudwatch_metric_alarm" "cloud_watch_alarm_req_per_target" {
   namespace           = "AWS/ApplicationELB"
   alarm_description   = "Alarm when come request to container"
   comparison_operator = "GreaterThanThreshold"
-  threshold           = 0
   dimensions = {
     TargetGroup = aws_alb_target_group.alb_target_grp.arn_suffix
   }
